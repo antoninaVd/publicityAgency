7088c5d689c9fbd4f1f39ec6dbd2980391c8dd75
Antonina Vdovichenko <a.vdovichenko@octetis.com>
2017-01-09 16:43:07 +0200

    create model Article

--- publicity_agency/models.py
+++ publicity_agency/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+
+class Article(models.Model):
+	title = models.CharField(max_length=250)
+	preview = models.TextField()
+	content = models.TextField()
+	date = models.DateTimeField(default=timezone.now)
+	source = models.CharField(max_length=500)
+
+def publish(self):
+	self.date = timezone.now()
+	self.save()
 
 # Create your models here.
